4845916b0fc3764b287d521720ece176c21c5bbf
Anthony Kiplimo <anthonylimo90@gmail.com>
2018-03-22 00:08:26 +0300

    Basic 8 Ball App
    This app tries to predict the future using a randomizing fucntion built into UIKit. Hope you can download it and enjoy it as well

--- 8-Ball/ViewController.swift
+++ 8-Ball/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImage: UIImageView!
+    
+    let imageArray = ["ball1","ball2","ball3","ball4","ball5"]
+    var randomBallNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        generateRandomImage()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func askAnything(_ sender: Any) {
+        generateRandomImage()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        generateRandomImage()
+    }
+    
+    func generateRandomImage () {
+        randomBallNumber = Int(arc4random_uniform(5))
+        ballImage.image = UIImage(named: imageArray[randomBallNumber])
+    }
 }
 
